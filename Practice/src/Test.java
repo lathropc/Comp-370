79a61feced3016c7da5b93732ef70ef65a86c8f2
Caleb Lathrop <lathropc@wit.edu>
2015-07-30 17:26:11 -0400

    Added random routes and shit

--- Practice/src/Test.java
+++ Practice/src/Test.java
@@ -2,17 +2,17 @@
 public class Test {
 
 	public static void main(String[] args) {
-		//Setup
+		
 		Tracks tracks = new Tracks(10, 10);
 		
 		train train = new train(1, 5, 5, true, 0);
 		tracks.addTrain(train);
 		
-		Routine moveTo = new MoveTo(7, 9);
-		train.setRoutine(moveTo);
+		Routine routine = new Repeat((new RandomRoute(tracks)));
+		train.setRoutine(routine);
 		System.out.println(train);
 		
-		//Execute 5 moves and print train out
+		
 		for (int i = 0; i < 5; i++) {
 			train.update();
 			System.out.println(train);
