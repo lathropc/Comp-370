d9ad1db57db4ec7b326671c72a9f20ff3fc38f60
Caleb Lathrop <lathropc@wit.edu>
2015-08-03 17:44:43 -0400

    asljfafd

---
+++ Practice/src/trains/IsTrainClose.java
@@ -0,0 +1,30 @@
+package trains;
+public class IsTrainClose extends Routine {
+
+	public IsTrainClose() {
+	}
+
+	@Override
+	public void reset() {
+		start();
+	}
+
+	@Override
+	public void act(train train, Tracks tracks) {
+		// figures out if train is approaching
+		for (train otherTrain : tracks.getTrains()) {
+			if (!train.getId().equals(otherTrain)) {
+				if (isApproaching(train, otherTrain)) {
+					succeed();
+					break;
+				}
+			}
+		}
+		fail();
+	}
+
+	private boolean isApproaching(train train, train otherTrain) {
+		return (Math.abs(train.getX() - otherTrain.getX()) <= 1 || Math
+				.abs(train.getY() - otherTrain.getY()) < 1);
+	}
+}
---
+++ Practice/src/trains/Test.java
@@ -0,0 +1,20 @@
+package trains;
+public class Test {
+
+	public static void main(String[] args) {
+
+		Tracks tracks = new Tracks(10, 10);
+
+		train train = new train("First Train", 5, 5, true, 0);
+		tracks.addTrain(train);
+
+		Routine routine = new Repeat((new RandomRoute(tracks)));
+		train.setRoutine(routine);
+		System.out.println(train);
+
+		for (int i = 0; i < 5; i++) {
+			train.update();
+			System.out.println(train);
+		}
+	}
+}
